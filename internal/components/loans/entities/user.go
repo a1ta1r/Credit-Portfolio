74fc5467d4ff37731307fa30b4d33124e524e465
Mikhail Sergeev <ima1ta1r@yandex.ru>
2019-02-10 19:02:17 +0500

    fixed deleted at in user hm

--- internal/components/loans/entities/user.go
+++ internal/components/loans/entities/user.go
@@ -10,7 +10,7 @@ type User struct {
 	ID           uint          `gorm:"primary_key" json:"id"`
 	CreatedAt    time.Time     `json:"-"`
 	UpdatedAt    time.Time     `json:"-"`
-	DeletedAt    time.Time     `json:"-"`
+	DeletedAt    *time.Time     `json:"-"`
 	Username     string        `json:"username" gorm:"type:varchar(100);unique_index"`
 	Email        string        `json:"email" gorm:"type:varchar(100);unique_index"`
 	Password     string        `json:"password,omitempty"`
